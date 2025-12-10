054942e72da2788f931cd64724843551dc37d124
Devansh Dayama <143751642+Devansh23dayama@users.noreply.github.com>
2025-12-10 16:09:25 +0530

    Time: 4 ms (78.77%) | Memory: 46.3 MB (29.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,22 +4,18 @@ class Solution {
         char newt[] = t.toCharArray();
         Arrays.sort(news);
         Arrays.sort(newt);
-        return Arrays.equals(news,newt);
-    //     int l1= news.length;
-    //     int l2= newt.length;
-    //     if(l1!=l2){
-    //         break;}
-           
-
-    // else{
-        
-    //         for(int i=0;i<l1;i++){
-    //              if(news[i]!=newt[i])
-    //              return false;
-    //         }
-    // }
-    //      return true;  
-    //     }
-        
+        // return Arrays.equals(news,newt);
+        int l1= news.length;
+        int l2= newt.length;
+        if(l1!=l2){
+            return false;
+        }
+        else{
+            for(int i=0;i<l1;i++){
+                 if(news[i]!=newt[i])
+                 return false;
+            }
+        }
+        return true;  
     }
 }
