9699bc39ac7649e4d38ede732efc6aafdd2290e5
Devansh Dayama <143751642+Devansh23dayama@users.noreply.github.com>
2026-02-03 16:42:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (32.11%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -1,10 +1,23 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        int odd = 1;
-      while(num>0){
-        num-=odd;
-        odd+=2;
-      }
-      return num==0;
+    //     int odd = 1;
+    //   while(num>0){
+    //     num-=odd;
+    //     odd+=2;
+    //   }
+    //   return num==0;
+
+     long min =1;
+    long max =num;
+   if(num==1) return true;
+    while(min<=max){
+       
+        long mid =min+(max-min)/2;
+         long sq=mid*mid;
+        if(sq==num)return true;
+        else if(mid*mid<num)min=mid+1;
+        else max=mid-1;
+    }
+return false;
     }
 }
