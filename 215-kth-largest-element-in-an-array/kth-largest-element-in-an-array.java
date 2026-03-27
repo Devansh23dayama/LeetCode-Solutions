5c2bbc9a59696f159767342b8665d3c2680f45c2
Devansh Dayama <143751642+Devansh23dayama@users.noreply.github.com>
2026-03-27 16:11:18 +0530

    Time: 67 ms (54.39%) | Memory: 68.7 MB (54.12%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,30 +1,13 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int temp = k ; 
-        for(int i = 0 ;i< nums.length; i ++){
-            if(temp>0){
-                pq.add(nums[i]);
-                temp--;
-            }
-            else{
-                if(pq.peek()<nums[i]){
-                    pq.poll();
-                    pq.add(nums[i]);
-                }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); 
+        for(int i : nums){
+            pq.add(i);
+            if(pq.size()>k){
+                pq.poll();
             }
         }
         return pq.peek();
-        
     }
-    static {
-        Runtime.getRuntime().gc();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter writer = new FileWriter("display_runtime.txt")) {
-                writer.write("0");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }));
-    }
+   
 }
