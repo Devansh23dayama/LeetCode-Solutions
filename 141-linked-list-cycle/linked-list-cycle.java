88d1a687d515d683ba741cc40f24fb1fb4838460
Devansh Dayama <143751642+Devansh23dayama@users.noreply.github.com>
2026-05-30 16:41:38 +0530

    Time: 7 ms (9.09%) | Memory: 46.5 MB (85.14%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,15 @@
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode curr=head;
+        while(curr!=null){
+            if(set.contains(curr)){
+                return true;
+            }
+            set.add(curr);
+            curr= curr.next;
+        }
+        return false;
+    }
+}
