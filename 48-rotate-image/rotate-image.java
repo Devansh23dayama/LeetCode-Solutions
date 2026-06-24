10fbc883dc29b592a67bb4a7be54d32220ecbf57
Devansh Dayama <143751642+Devansh23dayama@users.noreply.github.com>
2026-06-24 17:02:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (33.03%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,25 +1,26 @@
 class Solution {
     public void rotate(int[][] arr) {
-        int rowlength = arr.length;
-        int collength = arr[0].length;
+        int rowlength= arr.length;
+        int collength=arr[0].length;
+      for(int row = 0; row <rowlength;row++){
+        for(int col = row+1; col<collength;col++){
+            int temp = arr[row][col];
+            arr[row][col]=arr[col][row];
+            arr[col][row]=temp;
+        }
+      }
+      for(int row =0;row<rowlength;row++){
+        int start= 0;
+        int last =arr[row].length-1;
+        while(start<last){
+            int temp=arr[row][start];
+            arr[row][start]=arr[row][last];
+            arr[row][last]=temp;
+            start++;
+            last--;
+        }
+      }
+         
 
-        for (int row = 0; row < rowlength; row++) {
-            for (int col = row + 1; col < collength; col++) {
-                int temp = arr[row][col];
-                arr[row][col] = arr[col][row];
-                arr[col][row] = temp;
-            }
-        }
-        for (int row = 0; row < arr.length; row++) {
-            int startCol = 0;
-            int endCol = rowlength - 1;
-            while (startCol <= endCol) {
-              int temp= arr[row][startCol];
-               arr[row][startCol]=arr[row][endCol];
-               arr[row][endCol]=temp;
-                startCol++;
-                endCol--;
-            }
-        }
     }
 }
