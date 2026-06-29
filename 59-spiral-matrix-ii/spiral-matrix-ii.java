1e144e9671b7ac5ada4cd9ec6dd51edf4510fb35
Devansh Dayama <143751642+Devansh23dayama@users.noreply.github.com>
2026-06-29 18:00:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (62.25%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -1,39 +1,39 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int arr[][]= new int[n][n];
-        //m coloum, n-row
-        int num = 1;
-        int startRow = 0;
-        int endRow = n - 1;
-        int startCol = 0;
-        int endCol = n- 1;
+        int arr[][] = new int[n][n];
 
-        while (startRow <= endRow && startCol <= endCol) {
-            //Rowwise left to right means StartCol se endCol in row 
-            for (int m = startCol; m <= endCol; m++) {
-                arr[startRow][m] = num++;
+        int left = 0;
+        int right = n - 1;
+        int top = 0;
+        int bottom = n - 1;
+        int num = 1;
+        while(top<=bottom && left<=right){
+        for (int j = left; j <= right; j++) {
+            arr[top][j] = num;
+            num++;
+        }
+        top++;
+        for (int i = top; i <= bottom; i++) {
+            arr[i][right] = num;
+            num++;
+        }
+        right--;
+        if (top <= bottom) {
+            for (int j = right; j >= left; j--) {
+                arr[bottom][j] = num;
+                num++;
             }
-            startRow++;
-            // Col top to bottom means starting Row to ending Row 
-            for (int m = startRow; m <= endRow; m++) {
-                arr[m][endCol] = num++;
+            bottom--;
+        }
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                arr[i][left] = num;
+                num++;
             }
-            endCol--;
-            if (startRow <= endRow) {
-                //RowWide Risght to left end Row print end Col se start col 
-                for (int m = endCol; m >= startCol; m--) {
-                    arr[endRow][m] = num++;
-                }
-                endRow--;
-            }
-            //Col wise bottom to top startingCol print from ending row to start Row 
-            if (startCol <= endCol) {
-                for (int m = endRow; m >= startRow; m--) {
-                    arr[m][startCol] = num++;
-                }
-                startCol++;
-            }
+            left++;
+        }
         }
         return arr;
+
     }
 }
