d09eb406a14c693b4289b91724c1b4657465810e
Devansh Dayama <143751642+Devansh23dayama@users.noreply.github.com>
2026-06-30 11:31:37 +0530

    Time: 1 ms (99.87%) | Memory: 45.4 MB (48.87%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,27 +1,24 @@
 class Solution {
     public int compress(char[] chars) {
-        int i = 0, up = 0, n = chars.length;
+       int i = 0; 
+       int up =0;
+       int n= chars.length;
 
-        while (i < n) {
-            int j = i + 1; // this pointer will help to count same consecutive characters
+      while(i<n){
+        int j= i+1;
+        while(j<n && chars[j]==chars[i]) j++;
 
-            while (j < n && chars[j] == chars[i])
-                j++;
-
-            chars[up] = chars[i];
-            up++;
-
-            int cnt = j - i;
-            if (cnt > 1) {
-                for (char ch : String.valueOf(cnt).toCharArray()) {
-                    chars[up] = ch;
-                    up++;
-                }
+        chars[up]= chars[i];
+        up++;
+        int cnt = j-i;
+        if(cnt >1){
+            for(char ch : String.valueOf(cnt).toCharArray()){
+                chars[up]=ch;
+                up++;
             }
-
-            i = j; // putting i pointer on j i.e. from where the new character starts
         }
-        return up;
-
+        i=j;//iise taki vo dusre char ki value pe aa jaye 
+      }
+return up;
     }
 }
